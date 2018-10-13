d23afde32daea0c81db08bb834c1fb0e36735dc0
devparks <44073616+devparks@users.noreply.github.com>
2018-10-13 23:17:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 xhost +local:root
 
 docker run -it \
+--mount source=ros-catkin,target=/root ros \
 --env="DISPLAY"  \
 --env="QT_X11_NO_MITSHM=1"  \
 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
---volume="/home/$USER:/home/$USER" \
 --volume="/etc/group:/etc/group:ro" \
 --volume="/etc/passwd:/etc/passwd:ro" \
 --volume="/etc/shadow:/etc/shadow:ro" \
 --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
- ros-kinetic-gazebo
+ros-kinetic-gazebo
 
 xhost -local:root
