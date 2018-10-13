a46d03a9a5008da7bb35d931d557321b60fe4e5d
devparks <44073616+devparks@users.noreply.github.com>
2018-10-13 23:14:12 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+xhost +local:root
+
+docker run -it \
+--env="DISPLAY"  \
+--env="QT_X11_NO_MITSHM=1"  \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--volume="/home/$USER:/home/$USER" \
+--volume="/etc/group:/etc/group:ro" \
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
+--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+ ros-kinetic-gazebo
+
+xhost -local:root
