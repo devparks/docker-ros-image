7476c5979510862f6e62ad32abc6fced33a2ad19
devparks <44073616+devparks@users.noreply.github.com>
2018-10-14 12:27:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
-#!/bin/bash 
-xhost +local:root
+#!/bin/bash
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
+socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+open -g -a XQuartz
+xhost + $IP
 
 docker run -it \
---mount source=ros-catkin,target=/root ros \
---env="DISPLAY"  \
---env="QT_X11_NO_MITSHM=1"  \
---volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
---volume="/etc/group:/etc/group:ro" \
---volume="/etc/passwd:/etc/passwd:ro" \
---volume="/etc/shadow:/etc/shadow:ro" \
---volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-ros-kinetic-gazebo
+	-e DISPLAY=$IP:0 \
+	-e QT_X11_NO_MITSHM=1 \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	--privileged \
+	osrf/ros:indigo-desktop-full
 
-xhost -local:root
+xhost - $IP
+osascript -e 'quit app "XQuartz"'
+kill $(ps -ef | awk '/[s]ocat/{print $2}')
